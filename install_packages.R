2e9932ab534e4ec37be7d05f58428c3dc34c0e84
Matthias Orlowski <mace84@gmail.com>
2012-08-14 14:37:42 +0200

    Add plm package for panel econometrics

--- install_packages.R
+++ install_packages.R
@@ -6,7 +6,7 @@
 packages <- c(# general stuff 
               "foreign" , "xtable" , "reshape"
               # Analysis
-              , "MASS" , "Zelig" , "Amelia" , "gamlss" , "pcse" , "car", "lmtest", "Hmisc", "lmer", "nlme", "lme4"              
+              , "MASS" , "Zelig" , "Amelia" , "gamlss" , "pcse" , "car", "lmtest", "Hmisc", "lmer", "nlme", "lme4", "plm"              
               # graphics
               , "ggplot2" , "tikzDevice", "ROCR", "sparationplot", "latticeExtra", "gridExtra"
               # Bayesian analysis
